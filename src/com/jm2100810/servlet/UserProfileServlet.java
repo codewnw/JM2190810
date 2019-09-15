97dd93627fc9f1581f91fb5798f8d8f9d787c529
codewnw <code.wnw@gmail.com>
2019-09-15 11:05:37 +0530

    sendRedirect and forward

--- src/com/jm2100810/servlet/UserProfileServlet.java
+++ src/com/jm2100810/servlet/UserProfileServlet.java
@@ -3,38 +3,32 @@ package com.jm2100810.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/profile")
+public class UserProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public LoginServlet() {
+	public UserProfileServlet() {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("doGet of LoginServlet");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String username = request.getParameter("username");
-		String password = request.getParameter("password");
+		String msg = (String) request.getAttribute("msg");
 
-		if (username.equals(password)) {
-			request.setAttribute("msg", "Login Successfull.");
-			RequestDispatcher rd = request.getRequestDispatcher("profile");
-			rd.forward(request, response);
-		} else {
-			// response.sendRedirect("login.html");
-			response.sendRedirect("https://www.google.com/?search=Java");
-		}
+		response.setContentType("text/HTML");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Hi, " + username + "</h1>");
+		out.print("<p>" + msg + "</p>");
 	}
 
 }
